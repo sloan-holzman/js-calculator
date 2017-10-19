1b131232f401a075751f880738d1918b783adb32
Sloan Holzman <sloan.holzman@gmail.com>
2017-10-18 21:00:40 -0400

    write basic forms, but need to adjust for bonus

--- calculator.js
+++ calculator.js
@@ -1,2 +1,26 @@
+//need to build out the bonus
+
+var total = 0
+
 var calculator = {
+    add: function (a,b) {
+    let sum = a + b
+    console.log(sum)
+    return sum
+  },
+  subtract: function (a,b) {
+    let sub = a - b
+    console.log(sub)
+    return sub
+  },
+  divide: function (a,b) {
+    let division = a / b
+    console.log(division)
+    return division
+  },
+  exponentiate: function (a,b) {
+    let exponent = Math.pow(a,b)
+    console.log(exponent)
+    return exponent
+  }
 }
